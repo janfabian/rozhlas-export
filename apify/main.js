79229c7e1169d7bfad2d493369959ab70097f472
janfabian <jan.fabian@topmonks.com>
2020-09-01 12:23:46 +0200

    update main actor

--- apify/main.js
+++ apify/main.js
@@ -1,18 +1,57 @@
 const Apify = require("apify");
+const { log } = Apify.utils;
+
+const pageFunction = require("./pageFunction");
+
+const url =
+  "https://hledani.rozhlas.cz/iradio/?query=&reader=&offset=0&dateLimit=TL_1D";
 
 Apify.main(async () => {
-  const input = await Apify.getInput();
-  console.log("Input:");
-  console.dir(input);
+  const { categories } = await Apify.getInput();
+  console.log({ categories });
 
-  // Do something useful here...
+  const requestList = await Apify.openRequestList(
+    "rozhlas-categories",
+    categories.map((category) => {
+      const u = new URL(url);
+      u.searchParams.append("porad", category);
+      return u.toString();
+    })
+  );
 
-  // Save output
-  const output = {
-    receivedInput: input,
-    message: "Hello sir!",
-  };
-  console.log("Output:");
-  console.dir(output);
-  await Apify.setValue("OUTPUT", output);
+  const crawler = new Apify.CheerioCrawler({
+    requestList,
+    minConcurrency: 10,
+    maxConcurrency: 50,
+    maxRequestRetries: 1,
+    handlePageTimeoutSecs: 30,
+    maxRequestsPerCrawl: 10,
+    handlePageFunction: async ({ request, $ }) => {
+      log.debug(`Processing ${request.url}...`);
+
+      const episodeUrls = [];
+      $("#box-results h3 a").each((index, el) => {
+        episodeUrls.push({
+          url: el.href,
+        });
+      });
+
+      log.debug(episodeUrls);
+
+      const input = {
+        startUrls: episodeUrls.map((url) => {
+          url;
+        }),
+        pageFunction,
+      };
+
+      await Apify.call("apify/web-scraper", input);
+    },
+
+    handleFailedRequestFunction: async ({ request }) => {
+      log.debug(`Request ${request.url} failed twice.`);
+    },
+  });
+
+  await crawler.run();
 });
